64f19b47f5b3a06c288c4ea0fddd3ea32835f10c
Elod Tobak <tobakelod@gmail.com>
2022-10-06 21:24:01 +0200

    remove useless code and add some styling

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -3,22 +3,17 @@ import Canvas from "../game/canvas";
 
 interface IGameProps {}
 
-interface IGameState {
-  canvasRef: React.RefObject<Canvas>;
-}
+interface IGameState {}
 
 export default class Game extends React.Component<IGameProps, IGameState> {
   constructor(props: IGameProps) {
     super(props);
-    this.state = {
-      canvasRef: React.createRef<Canvas>(),
-    };
   }
 
   render() {
     return (
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-hidden">
-        <Canvas ref={this.state.canvasRef} width={800} height={800} />
+      <div className="card shadow-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-hidden">
+        <Canvas width={800} height={800} shootFrequency={1} fps={60} />
       </div>
     );
   }
