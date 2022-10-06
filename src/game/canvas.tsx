402335cd531662893eee0073b72efdc777427f61
Elod Tobak <tobakelod@gmail.com>
2022-10-06 21:22:41 +0200

    add automatic shooting, fps, fix animations, extract methods

--- src/game/canvas.tsx
+++ src/game/canvas.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 import Player from "./player";
 import BulletManager from "./bulletManager";
 import AlienManager from "./alienManager";
+import Hud from "./hud";
+import Menu from "./menu";
 
 interface ICanvasProps {
+  fps: number;
+  shootFrequency: number;
   width: number;
   height: number;
 }
@@ -11,9 +16,12 @@ interface ICanvasProps {
 interface ICanvasState {
   gameState: string;
   gameLoop: ReturnType<typeof setInterval> | null;
+  timer: number;
+  iterations: number;
   playerRef: React.RefObject<Player>;
   bulletManagerRef: React.RefObject<BulletManager>;
   alienManagerRef: React.RefObject<AlienManager>;
+  keys: ReturnType<typeof uuidv4>[];
 }
 
 export default class Canvas extends React.Component<
@@ -25,9 +33,12 @@ export default class Canvas extends React.Component<
     this.state = {
       gameState: "starting",
       gameLoop: null,
+      timer: 0,
+      iterations: 0,
       playerRef: React.createRef<Player>(),
       bulletManagerRef: React.createRef<BulletManager>(),
       alienManagerRef: React.createRef<AlienManager>(),
+      keys: [uuidv4(), uuidv4(), uuidv4()],
     };
   }
 
@@ -42,26 +53,61 @@ export default class Canvas extends React.Component<
     }
   }
 
+  resetKeys() {
+    this.setState({ keys: [uuidv4(), uuidv4(), uuidv4()] });
+  }
+
   listenForEnter() {
     document.addEventListener("keydown", (e) => this.startGame(e));
   }
 
   startGame(event: KeyboardEvent) {
-    if (event.key === "Enter") {
-      const startScreen = document.getElementById("startScreen");
+    if (
+      event.key === "Enter" &&
+      "starting win lose".includes(this.state.gameState)
+    ) {
+      const menu = document.getElementById("menu");
       const canvas = document.getElementById("canvas");
+      const hud = document.getElementById("hud");
 
-      startScreen?.classList.add("animate-fade-out");
+      if (!menu || !canvas || !hud) {
+        return;
+      }
 
-      startScreen?.addEventListener("animationend", () => {
-        canvas?.classList.add("animate-sharpen-in");
-      });
+      menu.classList.remove("animate-fade-in");
+      menu.classList.add("animate-fade-out");
 
-      canvas?.addEventListener("animationend", () => {
-        this.setState({ gameState: "playing" });
-      });
+      setTimeout(() => {
+        canvas.classList.remove("animate-sharpen-out");
+        canvas.classList.add("animate-sharpen-in");
+
+        hud.classList.remove("animate-sharpen-out");
+        hud.classList.add("animate-sharpen-in");
+      }, 1500);
+
+      setTimeout(() => {
+        this.setState({ gameState: "playing", timer: 0 });
+      }, 3000);
     }
-    document.removeEventListener("keydown", (e) => this.startGame(e));
+  }
+
+  animateGameEnd() {
+    const menu = document.getElementById("menu");
+    const canvas = document.getElementById("canvas");
+    const hud = document.getElementById("hud");
+
+    if (!menu || !canvas || !hud) {
+      return;
+    }
+
+    menu.classList.remove("animate-fade-out");
+    menu.classList.add("animate-fade-in");
+
+    hud.classList.add("animate-sharpen-out");
+    hud.classList.remove("animate-sharpen-in");
+
+    canvas.classList.add("animate-sharpen-out");
+    canvas.classList.remove("animate-sharpen-in");
   }
 
   update() {
@@ -71,26 +117,40 @@ export default class Canvas extends React.Component<
           this.state.playerRef.current?.update();
           this.state.bulletManagerRef.current?.update();
           this.state.alienManagerRef.current?.update();
+
+          if (
+            this.state.iterations %
+              (this.props.fps / this.props.shootFrequency) ===
+            0
+          ) {
+            this.state.bulletManagerRef.current?.shoot();
+          }
+
+          if (this.state.alienManagerRef.current?.state.aliens.length === 0) {
+            this.setState({ gameState: "win" });
+            this.animateGameEnd();
+            this.resetKeys();
+          }
+
+          if (
+            this.state.playerRef.current?.state.lives === 0 ||
+            this.state.alienManagerRef.current?.getLowestY()! <=
+              this.state.playerRef.current?.state.y! + 50
+          ) {
+            this.setState({ gameState: "lose" });
+            this.animateGameEnd();
+            this.resetKeys();
+          }
+
+          this.setState({
+            timer: this.state.timer + 1000 / this.props.fps,
+            iterations:
+              (this.state.iterations + 1) %
+              (this.props.fps / this.props.shootFrequency),
+          });
         }
-      }, 20),
+      }, 1000 / this.props.fps),
     });
-  }
-
-  renderStartingScreen() {
-    return (
-      <div id="startScreen">
-        <div className="absolute w-72 h-72 bg-lime-400 rounded-full mix-blend-lighter filter blur-xl opacity-70 animate-blob top-[35%] left-[20%]"></div>
-        <div className="absolute w-72 h-72 bg-green-400 rounded-full mix-blend-lighter filter blur-xl opacity-70 animate-blob animation-delay-8000 top-[40%] left-[30%]"></div>
-        <div className="absolute w-72 h-72 bg-yellow-400 rounded-full mix-blend-lighter filter blur-xl opacity-70 animate-blob animation-delay-4000 top-[35%] left-[40%]"></div>
-
-        <div className="absolute bg-black text-white bg-opacity-90 rounded-full py-4 px-10 w-max text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <h1 className="text-4xl font-bold mb-3">Space Invaders</h1>
-          <span className="text-xl">Press</span>
-          <kbd className="text-xl kbd mx-1">Enter</kbd>
-          <span className="text-xl mr-2">to start</span>
-        </div>
-      </div>
-    );
   }
 
   render() {
@@ -98,16 +158,14 @@ export default class Canvas extends React.Component<
       <div>
         <div
           id="canvas"
-          className={
-            (this.state.gameState == "starting" ? "blur" : "") +
-            " relative bg-black"
-          }
+          className="blur relative bg-black"
           style={{
             width: this.props.width,
             height: this.props.height,
           }}
         >
           <Player
+            key={this.state.keys[0]}
             ref={this.state.playerRef}
             x={this.props.width / 2 - 25}
             y={50}
@@ -115,6 +173,7 @@ export default class Canvas extends React.Component<
           />
 
           <BulletManager
+            key={this.state.keys[1]}
             ref={this.state.bulletManagerRef}
             canvasHeight={this.props.height}
             canvasWidth={this.props.width}
@@ -122,6 +181,7 @@ export default class Canvas extends React.Component<
           />
 
           <AlienManager
+            key={this.state.keys[2]}
             ref={this.state.alienManagerRef}
             canvasHeight={this.props.height}
             canvasWidth={this.props.width}
@@ -129,9 +189,13 @@ export default class Canvas extends React.Component<
             bulletManagerRef={this.state.bulletManagerRef}
           />
         </div>
-        {this.state.gameState === "starting"
-          ? this.renderStartingScreen()
-          : null}
+
+        <Menu gameState={this.state.gameState} timer={this.state.timer} />
+        <Hud
+          score={this.state.alienManagerRef.current?.state.aliens.length || 0}
+          timer={this.state.timer / 1000}
+          lives={this.state.playerRef.current?.state.lives || 0}
+        />
       </div>
     );
   }
