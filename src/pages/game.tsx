83b5b2cd2daf2d969b163ab81788f8d61171e26d
Elod Tobak <tobakelod@gmail.com>
2022-10-08 17:46:03 +0200

    add control and volume options

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -13,7 +13,14 @@ export default class Game extends React.Component<IGameProps, IGameState> {
   render() {
     return (
       <div className="card shadow-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-hidden">
-        <Canvas width={800} height={800} shootFrequency={1} fps={60} />
+        <Canvas
+          width={800}
+          height={800}
+          shootFrequency={1}
+          fps={60}
+          control="tensorflow"
+          volume={0.3}
+        />
       </div>
     );
   }
